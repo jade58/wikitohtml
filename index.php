bc8c80405c8a0c944120faf9ae3c9e306643eaaa
jadewizard <buryakovdima@yandex.ru>
2015-07-06 09:33:15 +0300

    tmp

--- index.php
+++ index.php
@@ -1,83 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-<title>Bootstrap 101 Template</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Bootstrap 101 Template</title>
 
-<!-- Bootstrap -->
-<link href="css/bootstrap.css" rel="stylesheet">
-<link href="css/style.css" rel="stylesheet">
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
 
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
   <body>
-<div class="s_content">
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Wiki To Html</a>
+    <div class="s_content">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Wiki To Html</a>
+          </div>
+
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Авторизироваться через ВКонтакте</a></li>
+            </ul>
+          </div>
         </div>
+      </nav>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-               
-          </ul>
-          
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Авторизироваться через ВКонтакте</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
 
-    <div class="sidebar">
 
-      <div class="panel panel-default">
-        <div class="panel-heading">Параметры</div>
-        <div class="panel-body">
-          <form method="get" action="index.php">
 
-           <div class="form-group">
-             <label class="control-label" for="focusedInput">URL страницы</label>
-             <input class="form-control" id="focusedInput" type="text">
-           </div>
 
-           <input class="btn btn-default" type="submit" value="Получить код">
 
-         </form>
+
+
+      <div class="row">
+
+      <div class="col-md-3">      
+         <div class="panel panel-default">
+          <div class="panel-heading">Параметры</div>
+          <div class="panel-body">
+            <form method="get" action="index.php">
+
+             <div class="form-group">
+               <label class="control-label" for="focusedInput">URL страницы</label>
+               <input class="form-control" id="focusedInput" type="text">
+             </div>
+
+             <input class="btn btn-default" type="submit" value="Получить код">
+
+           </form>
+         </div>
        </div>
-     </div>
 
-   </div>
+       <div class="panel panel-default">
+          <div class="panel-heading">Контакты</div>
+          <div class="panel-body">
+            
+         </div>
+       </div>
+      </div>
 
-   <div class="code">
 
-     <div class="well">
-       
+
+       <div class="col-md-9"> <div class="well">
+
         <div class="form-group">
-     
 
-        <textarea class="form-control" rows="20" id="textArea"></textarea>
- 
+
+          <textarea class="form-control" rows="20" id="textArea"></textarea>
+
+        </div>
+
+      </div></div>
     </div>
 
-    </div>
-
-  </div>
   </div>
 
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
